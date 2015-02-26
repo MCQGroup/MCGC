477554e01d9a86dc8676a25ec4136877348dd41f
SLAPaper <slapaper.pku@gmail.com>
2015-02-27 01:00:19 +0800

    finished spsummon limit

--- c284130812.lua
+++ c284130812.lua
@@ -1,20 +1,20 @@
 -- MC群的群主 纸睡
 function c284130812.initial_effect(c)
-    --通常召唤
-	local e1 = Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_LIMIT_SUMMON_PROC)
-	e1:SetCondition(c284130812.summonCondition)
-	e1:SetOperation(c284130812.ttop)
-	e1:SetValue(SUMMON_TYPE_ADVANCE)
-	c:RegisterEffect(e1)
+    -- 通常召唤
+    local e1 = Effect.CreateEffect(c)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_LIMIT_SUMMON_PROC)
+    e1:SetCondition(c284130812.summonCondition)
+    e1:SetOperation(c284130812.summonOperation)
+    e1:SetValue(SUMMON_TYPE_ADVANCE)
+    c:RegisterEffect(e1)
 
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_LIMIT_SET_PROC)
-	c:RegisterEffect(e2)
+    local e2 = e1:Clone()
+    e2:SetCode(EFFECT_LIMIT_SET_PROC)
+    c:RegisterEffect(e2)
 
-    --特殊召唤
+    -- 特殊召唤
     local e3 = Effect.CreateEffect(c)
     e3:SetType(EFFECT_TYPE_FIELD)
     e3:SetCode(EFFECT_SPSUMMON_PROC)
@@ -22,38 +22,49 @@ function c284130812.initial_effect(c)
     e3:SetRange(LOCATION_HAND)
     e3:SetCondition(c284130812.spSummonCondition)
     e3:SetOperation(c284130812.spSummonOperation)
+    e3:SetValue(SUMMON_TYPE_SPECIAL + 0x2222222)
     c:RegisterEffect(e3)
-    
+
+    local e4 = Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE)
+    e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    e4:SetCode(EFFECT_SPSUMMON_CONDITION)
+    SetValue(c284130812.spSummonLimit)
+
 end
 
 function c284130812.filter(c)
     return c.IsSetCard(0x2222)
 end
 
-function c284130812.summonCondition(e,c)
-	if c == nil then
+function c284130812.summonCondition(e, c)
+    if c == nil then
         return true
     end
     local tributeCount = 3 - Duel.GetMatchingGroupCount(c284130812.filter, c.GetController(), LOCATION_GRAVE, 0, nil)
     if tributeCount < 0 then
         tributeCount = 0
     end
-	return Duel.GetTributeCount(c) >= tributeCount
+    return Duel.GetTributeCount(c) >= tributeCount
 end
 
-function c284130812.summonOperation(e,tp,eg,ep,ev,re,r,rp,c)
+function c284130812.summonOperation(e, tp, eg, ep, ev, re, r, rp, c)
     local tributeCount = 3 - Duel.GetMatchingGroupCount(c284130812.filter, c.GetController(), LOCATION_GRAVE, 0, nil)
     if tributeCount < 0 then
         tributeCount = 0
     else
-	    local g = Duel.SelectTribute(tp,c,tributeCount,tributeCount)
-	    c:SetMaterial(g)
-	    Duel.Release(g, REASON_SUMMON+REASON_MATERIAL)
+        local g = Duel.SelectTribute(tp, c, tributeCount, tributeCount)
+        c:SetMaterial(g)
+        Duel.Release(g, REASON_SUMMON + REASON_MATERIAL)
     end
 end
 
-function c284130812.spSummonCondition(e,c)
+function c284130812.spSummonCondition(e, c)
 end
 
-function c284130812.spSummonOperation(e,tp,eg,ep,ev,re,r,rp,c)
+function c284130812.spSummonOperation(e, tp, eg, ep, ev, re, r, rp, c)
+end
+
+function c284130812.spSummonLimit(e, se, sp, st)
+    return bit.band(st, SUMMON_TYPE_SPECIAL + 0x2222222) == SUMMON_TYPE_SPECIAL + 0x2222222
 end
