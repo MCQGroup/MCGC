5ebe487220f1c78c5280a18aeb18869ff3d44aa1
SLAPaper <slapaper.pku@gmail.com>
2015-02-27 10:30:13 +0800

    doing with spSummonFromHandCost

--- c284130812.lua
+++ c284130812.lua
@@ -29,8 +29,28 @@ function c284130812.initial_effect(c)
     e4:SetType(EFFECT_TYPE_SINGLE)
     e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
     e4:SetCode(EFFECT_SPSUMMON_CONDITION)
-    SetValue(c284130812.spSummonLimit)
+    e4:SetValue(c284130812.spSummonLimit)
+    c:RegisterEffect(e4)
 
+    -- ¿¨×é¼ìË÷
+    local e5 = Effect.CreateEffect(c)
+    e5:SetCategory(CATEGORY_SEARCH + CATEGORY_TOHAND)
+    e5:SetType(EFFECT_TYPE_TRIGGER_F + EFFECT_TYPE_SINGLE)
+    e5:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e5:SetTarget(c284130812.deckSearchTarget)
+    e5:SetOperation(c284130812.deckSearchOperation)
+    c:RegisterEffect(e5)
+
+    -- ÊÖ¿¨ÕÙ»½
+    local e6 = Effect.CreateEffect(c)
+    e6:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_DAMAGE)
+    e6:SetType(EFFECT_TYPE_TRIGGER_F + EFFECT_TYPE_SINGLE)
+    e6:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e6:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e6:SetTarget(c284130812.spSummonFromHandTarget)
+    e6:SetOperation(c284130812.spSummonFromHandOperation)
+    e6:SetCost(c284130812.spSummonFromHandCost)
+    c:RegisterEffect(e6)
 end
 
 function c284130812.filter(c)
@@ -45,7 +65,7 @@ function c284130812.summonCondition(e, c)
     if tributeCount < 0 then
         tributeCount = 0
     end
-    return Duel.GetTributeCount(c) >= tributeCount
+    return Duel.GetTributeCount(c) >= tributeCount and Duel.GetLocationCount(tp, LOCATION_MZONE)
 end
 
 function c284130812.summonOperation(e, tp, eg, ep, ev, re, r, rp, c)
@@ -60,11 +80,61 @@ function c284130812.summonOperation(e, tp, eg, ep, ev, re, r, rp, c)
 end
 
 function c284130812.spSummonCondition(e, c)
+    if c == nil then
+        return true
+    end
+    local targetPlayer = c:GetControler()
+    return Duel.GetLocationCount(targetPlayer, LOCATION_MZONE) > 0
+    and Duel.IsExistingMatchingCard(c284130812.filter, targetPlayer, LOCATION_GRAVE, 0, 1, nil)
 end
 
 function c284130812.spSummonOperation(e, tp, eg, ep, ev, re, r, rp, c)
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_REMOVE)
+    local g = Duel.SelectMatchingCard(tp, c284130812.filter, tp, LOCATION_GRAVE, 0, 1, 1, nil)
+    Duel.Remove(g, POS_FACEUP, REASON_COST)
 end
 
 function c284130812.spSummonLimit(e, se, sp, st)
     return bit.band(st, SUMMON_TYPE_SPECIAL + 0x2222222) == SUMMON_TYPE_SPECIAL + 0x2222222
+end
+
+function c284130812.deckSearchTarget(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(c284130812.filter, tp, LOCATION_DECK, 0, 1, nil)
+    end
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK)
+end
+
+function c284130812.deckSearchOperation(e, tp, eg, ep, ev, re, r, rp)
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
+    local g = Duel.SelectMatchingCard(tp, c284130812.filter, tp, LOCATION_DECK, 0, 1, 1, nil)
+    if g:getCount() > 0 then
+        Duel.SendtoHand(g, nil, REASON_EFFECT)
+        Duel.ConfirmCards(1 - tp, g)
+    end
+end
+
+function c284130812.spSummonFromHandTarget(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(c284130812.filter, tp, LOCATION_HAND, 0, 1, nil) and Duel.GetLocationCount(tp, LOCATION_MZONE) > 0
+    end
+    local g = Duel.GetMatchingGroup(c284130812.filter, tp, LOCATION_HAND, 0, nil)
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, g, g:GetCount(), 0, 0)
+end
+
+function c284130812.spSummonFromHandOperation(e, tp, eg, ep, ev, re, r, rp)
+    local g = Duel.SelectMatchingCard(tp, c284130812.spSummonFromHandFilter, tp, LOCATION_HAND, 0, 0, Duel.GetLocationCount(tp, LOCATION_MZONE), nil, e, tp)
+    Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP)
+end
+
+function c284130812.spSummonFromHandFilter(c, e, tp)
+    return c284130812.filter(c) and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+end
+
+function c284130812.spSummonFromHandCost(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.CheckLPCost(tp, 0)
+    else
+        Duel.PayLPCost(tp, 0)
+    end
 end
