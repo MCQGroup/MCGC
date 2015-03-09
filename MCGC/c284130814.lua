4cf4d6b0f4d4d0100d1e948cb65755536d70f9a9
SLAPaper <slapaper.pku@gmail.com>
2015-03-09 20:24:48 +0800

    beginning with c284130814

--- MCGC/c284130814.lua
+++ MCGC/c284130814.lua
@@ -1,6 +1,10 @@
 -- 植吧MC群
 function c284130814.initial_effect(c)
     -- 返回卡组
+    local e1 = Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TODECK)
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_ACTIVATE + EFFECT_TYPE_TRIGGER_F)
+    e1:SetCode(EVENT_CHAINING)
     -- 抽卡
     -- 防止破坏
     -- 维持代价
