3b9ff6be990cab4d4f1133a4c2b875d919655385
SLAPaper <slapaper.pku@gmail.com>
2015-03-12 23:31:56 +0800

    finish effect2

--- MCGC/c284130814.lua
+++ MCGC/c284130814.lua
@@ -6,9 +6,18 @@ function c284130814.initial_effect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetOperation(c284130814.toDeckOperation)
+    c:RegisterEffect(e1)
 
     -- 防止破坏
     local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetRange(LOCATION_SZONE)
+    e2:SetTargetRange(LOCATION_MZONE, 0)
+    e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard, 0x2222))
+    e2:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+    e2:SetCountLimit(1)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
 
     -- 维持代价
 end
