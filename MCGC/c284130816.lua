6a947084953865639a1a75a07f2fb079e46549cd
SLAPaper <slapaper.pku@gmail.com>
2015-03-17 22:41:45 +0800

    doing with c284130816

--- MCGC/c284130816.lua
+++ MCGC/c284130816.lua
@@ -1,5 +1,16 @@
 -- MC群的软妹 玲音
 
 function c284130816.initial_effect(c)
-    
+    -- 灵摆召唤
+    aux.AddPendulumProcedure(c)
+    -- 发动
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(e1)
+    -- 特招限制
+end
+
+function c284130816.filter(c)
+    return c:IsSetCard(0x2222)
 end
