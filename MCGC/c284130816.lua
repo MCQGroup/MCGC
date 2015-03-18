0c9a412eda4fd0602e45d5dbe05e6e67603570be
SLAPaper <slapaper.pku@gmail.com>
2015-03-18 22:33:17 +0800

    doing with c284130816:e2

--- MCGC/c284130816.lua
+++ MCGC/c284130816.lua
@@ -8,9 +8,36 @@ function c284130816.initial_effect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(e1)
+
     -- 特招限制
+    local e2 = Effect.CreateEffect(c)
+    e2:SetRange(LOCATION_PZONE)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_CANNOT_DISABLE)
+    e2:SetTargetRange(1, 0)
+    e2:SetTarget(c284130816.specialSummonLimit)
+    c:RegisterEffect(e2)
+
+    -- 除外抽卡
+    local e3 = Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_REMOVE + CATEGORY_DRAW)
+    c:RegisterEffect(e3)
+
+    -- 无祭品普招
+    local e4 = Effect.CreateEffect(c)
+    c:RegisterEffect(e4)
+
+    -- 加入手卡
+    local e5 = Effect.CreateEffect(c)
+    e5:SetCategory(CATEGORY_TOHAND)
+    c:RegisterEffect(e5)
 end
 
 function c284130816.filter(c)
     return c:IsSetCard(0x2222)
+end
+
+function c284130816.specialSummonLimit(e, c)
+    return not c284130816.filter(c)
 end
