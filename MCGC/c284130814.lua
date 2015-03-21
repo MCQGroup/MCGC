fbdd7fa1819bb00bc983ddca12f65553e40df986
SLAPaper <slapaper.pku@gmail.com>
2015-03-21 14:52:27 +0800

    add activate check to c284130814

--- MCGC/c284130814.lua
+++ MCGC/c284130814.lua
@@ -3,8 +3,10 @@ function c284130814.initial_effect(c)
     -- 返回卡组、抽卡
     local e1 = Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_TODECK + CATEGORY_DRAW)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetTarget(c284130814.toDeckTarget)
     e1:SetOperation(c284130814.toDeckOperation)
     c:RegisterEffect(e1)
 
@@ -35,6 +37,18 @@ function c284130814.filter(c)
     return c:IsSetCard(0x2222)
 end
 
+function c284130814.toDeckTarget(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return tp:IsPlayerCanDraw(tp) and Duel.IsExistingTarget(c284130814.filter, tp, LOCATION_GRAVE, 0, 1, nil) and Duel.IsExistingTarget(c284130814.filter, tp, LOCATION_REMOVED, 0, 1, nil) and Duel.IsExistingTarget(c284130814.filter, tp, LOCATION_HAND, 0, 1, e:GetHandler())
+    end
+    local g1 = Duel.GetMatchingGroup(c284130814.filter, tp, LOCATION_HAND, 0, e:GetHandler())
+    local g2 = Duel.GetMatchingGroup(c284130814.filter, tp, LOCATION_REMOVED, 0, nil)
+    local g3 = Duel.GetMatchingGroup(c284130814.filter, tp, LOCATION_GRAVE, 0, nil)
+    local g = Group.CreateGroup()
+    g:Merge(g1):Merge(g2):Merge(g3)
+    Duel.SetOperationInfo(0, CATEGORY_TODECK, g, g:GetCount(), 0, 0)
+end
+
 function c284130814.toDeckOperation(e, tp, eg, ep, ev, re, r, rp)
     local g = Duel.GetMatchingGroup(c284130814.filter, tp, LOCATION_HAND + LOCATION_GRAVE + LOCATION_REMOVED, 0, nil)
     Duel.SendtoDeck(g, tp, nil, REASON_EFFECT)
@@ -45,7 +59,7 @@ function c284130814.costOperation(e, tp, eg, ep, ev, re, r, rp)
     if Duel.GetTurnPlayer() ~= tp then
         return
     end
-    if Duel.GetLP(tp) > 1000 and Duel.SelectYesNo(tp, aux.Stringid(284130814, 0)) then
+    if Duel.CheckLPCost(tp, 1000) and Duel.SelectYesNo(tp, aux.Stringid(284130814, 0)) then
         Duel.PayLPCost(tp, 1000)
     else
         Duel.Remove(e:GetHandler(), REASON_RULE)
