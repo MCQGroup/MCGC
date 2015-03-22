4d17a14caa142fcd8154b632e7d883a46d5c4a46
SLAPaper <slapaper.pku@gmail.com>
2015-03-22 20:29:14 +0800

    minor fix c284130814

--- MCGC/c284130814.lua
+++ MCGC/c284130814.lua
@@ -48,13 +48,13 @@ function c284130814.toDeckTarget(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     local g2 = Duel.GetMatchingGroup(c284130814.filter, tp, LOCATION_REMOVED, 0, nil)
     local g3 = Duel.GetMatchingGroup(c284130814.filter, tp, LOCATION_GRAVE, 0, nil)
     local g = Group.CreateGroup()
-    if not g1 then
+    if g1 then
         g:Merge(g1)
     end
-    if not g2 then
+    if g2 then
         g:Merge(g2)
     end
-    if not g3 then
+    if g3 then
         g:Merge(g3)
     end
     Duel.SetOperationInfo(0, CATEGORY_TODECK, g, g:GetCount(), 0, 0)
