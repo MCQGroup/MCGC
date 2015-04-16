73014741bd00dade0b91ccc7325de304998178de
SLAPaper <slapaper.pku@gmail.com>
2015-04-16 22:39:08 +0800

    finished c284130870 effect2

--- MCGC/c284130870.lua
+++ MCGC/c284130870.lua
@@ -14,7 +14,7 @@ function c284130870.initial_effect(c)
 
     -- 手卡丢弃触发卡组检索
     local e2 = Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_SEARCH)
+    e2:SetCategory(CATEGORY_SEARCH + CATEGORY_TOHAND)
     e2:SetRange(LOCATION_HAND)
     e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetCost(c284130870.toGraveCost)
@@ -38,6 +38,10 @@ end
 function c284130870.removeOperation(e, tp, eg, ep, ev, re, r, rp)
 end
 
+function c284130870.toGraveFilter(c)
+    return c:IsCode(284130870) or c:IsCode(284130826)
+end
+
 function c284130870.toGraveCost(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
         return e:GetHandler():IsAbleToGraveAsCost()
@@ -47,8 +51,13 @@ end
 
 function c284130870.toGraveTarget(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     if chk == 0 then
+        return Duel.IsExistingMatchingCard(c284130870.toGraveFilter, tp, LOCATION_DECK, 0, 1, nil)
     end
+    local g = Duel.SelectTarget(tp, c284130870.toGraveFilter, tp, LOCATION_DECK, 0, 1, 1, nil)
+    Duel.SetOperationInfo(0, CATEGORY_SEARCH + CATEGORY_TOHAND, g, g:GetCount(), nil, 0)
 end
 
 function c284130870.toGraveOperation(e, tp, eg, ep, ev, re, r, rp)
+    local g = Duel.GetChainInfo(0, CHAININFO_TARGET_CARDS)
+    Duel.SendtoHand(g, tp, REASON_EFFECT)
 end
