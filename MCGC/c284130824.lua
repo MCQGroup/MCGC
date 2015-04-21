ecb1c35f88ce862becf58eae7c1329c30ac5d0d3
SLAPaper <slapaper.pku@gmail.com>
2015-04-21 21:54:55 +0800

    adding special summon progress to c284130824

--- MCGC/c284130824.lua
+++ MCGC/c284130824.lua
@@ -4,9 +4,17 @@ function c284130824.initial_effect(c)
     c:EnableReviveLimit()
 
     -- 特招方法
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_SPSUMMON_PROC)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 
+    c:RegisterEffect(e1)
 
     -- 特招触发（无法连锁）
 
     -- 对方额外抽卡触发
+end
+
+function c284130824.summonFilter(c)
+    return c:IsSetCard(0x2222) and c:IsType(TYPE_MONSTER)
 end
