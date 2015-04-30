c09529f3ecc902ba482491be21fbee0960f87b4c
SLAPaper <slapaper.pku@gmail.com>
2015-04-30 23:24:19 +0800

    doing with c284130825, minor modified c284130814

--- MCGC/c284130825.lua
+++ MCGC/c284130825.lua
@@ -19,6 +19,16 @@ function c284130825.initial_effect(c)
     c:RegisterEffect(e2)
 
     -- 回复
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetCategory(CATEGORY_TODECK + CATEGORY_RECOVER + CATEGORY_SEARCH + CATEGORY_TOHAND)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e3:SetRange(LOCATION_PZONE)
+    e3:SetCountLimit(1)
+    e3:SetCost(c284130816.drawCost)
+    e3:SetTarget(c284130816.drawTarget)
+    e3:SetOperation(c284130816.drawOperation)
+    c:RegisterEffect(e3)
 
     -- 特招规则
 
@@ -32,4 +42,27 @@ end
 
 function c284130825.specialSummonLimit(e, c)
     return not c284130825.filter(c)
+end
+
+function c284130825.recoverCost(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(aux.TRUE, tp, LOCATION_HAND, 0, 2, nil)
+    end
+    local g = Duel.SelectMatchingCard(tp, aux.TRUE, tp, LOCATION_HAND, 0, 2, 2, nil)
+    Duel.SendtoDeck(g, nil, 2, REASON_COST)
+    Duel.ShuffleDeck(tp)
+end
+
+function c284130825.recoverTarget(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return true
+    end
+    Duel.SetTargetPlayer(tp)
+    Duel.SetTargetParam(1000)
+    Duel.SetOperationInfo(0, CATEGORY_RECOVER, nil, 0, tp, 1000)
+end
+
+function c284130825.recoverOperation(e, tp, eg, ep, ev, re, r, rp)
+    Duel.Recover(tp, 1000, REASON_EFFECT)
+    local g = Duel.SelectMatchingCard(tp, aux.TRUE, tp, LOCATION_EXTRA, 0, 1, 1, nil)
 end
