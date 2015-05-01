81bf79fa2c8ba9571bcb3ffe9b5090763c116eb7
SLAPaper <slapaper.pku@gmail.com>
2015-05-01 14:11:01 +0800

    modified c284130864, doing with c284130824

--- MCGC/c284130824.lua
+++ MCGC/c284130824.lua
@@ -7,15 +7,15 @@ function c284130824.initial_effect(c)
     c:EnableReviveLimit()
 
     -- 融合素材
-    aux.AddFusionProcFunRep(c, c284130824.summonFilter, 2, true)
+    aux.AddFusionProcFunRep(c, c284130824.spsummonFilter, 2, true)
 
     -- 特招限制
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
     e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-    e1:SetValue(c284130824.summonLimit)
-    c:RegisterEffect(e1)
+    e1:SetValue(c284130824.spsummonLimit)
+    --    c:RegisterEffect(e1)
 
     -- 特招方法
     local e2 = Effect.CreateEffect(c)
@@ -23,8 +23,8 @@ function c284130824.initial_effect(c)
     e2:SetCode(EFFECT_SPSUMMON_PROC)
     e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
     e2:SetRange(LOCATION_EXTRA)
-    e2:SetCondition(c284130824.summonCondition)
-    e2:SetOperation(c284130824.summonOperation)
+    e2:SetCondition(c284130824.spsummonCondition)
+    e2:SetOperation(c284130824.spsummonOperation)
     c:RegisterEffect(e2)
 
     -- 特招触发（无法连锁）
@@ -49,43 +49,31 @@ function c284130824.initial_effect(c)
     c:RegisterEffect(e4)
 end
 
-function c284130824.summonFilter(c)
+function c284130824.spsummonFilter(c)
     return c:IsSetCard(0x2222) and c:IsType(TYPE_MONSTER) and c:IsFaceup()
 end
 
-function c284130824.summonLimit(e, se, sp, st)
-    Debug.Message("####调试信息####")
-    Debug.Message("summonLimit")
-    Debug.Message(e:GetHandler():IsLocation(LOCATION_EXTRA))
-    Debug.Message("################")
-    -- return not e:GetHandler():IsLocation(LOCATION_EXTRA)
+function c284130824.spsummonLimit(e, se, sp, st)
+    return not e:GetHandler():IsLocation(LOCATION_EXTRA)
     -- 限制“从额外卡组以外的地方”召唤
-    return false
 end
 
-function c284130824.summonCondition(e, c)
+function c284130824.spsummonCondition(e, c)
     if c == nil then
         return ture
     end
     local tp = c:GetControler()
-    Debug.Message("####调试信息####")
-    Debug.Message("GetLocationCount")
-    Debug.Message(Duel.GetLocationCount(tp, LOCATION_MZONE))
-    Debug.Message("IsExistingMatchingCard")
-    Debug.Message(Duel.IsExistingMatchingCard(c284130824.summonFilter, tp, LOCATION_ONFIELD, 0, 2, nil))
-    Debug.Message("################")
-    --[[ return Duel.GetLocationCount(tp, LOCATION_MZONE) > -2
+    return Duel.GetLocationCount(tp, LOCATION_MZONE) > -2
     -- 我能说这个场地剩余空格大于一个负数是什么意思我没看懂吗
 
-    and Duel.IsExistingMatchingCard(c284130824.summonFilter, tp, LOCATION_ONFIELD, 0, 2, nil)]]
-    return true
+    and Duel.IsExistingMatchingCard(c284130824.spsummonFilter, tp, LOCATION_ONFIELD, 0, 2, nil)
 end
 
-function c284130824.summonOperation(e, tp, eg, ep, ev, re, r, rp, c)
+function c284130824.spsummonOperation(e, tp, eg, ep, ev, re, r, rp, c)
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TODECK)
-    local g = Duel.SelectMatchingCard(tp, c284130824.summonFilter, tp, LOCATION_MZONE, 0, 2, 2, nil)
+    local g = Duel.SelectMatchingCard(tp, c284130824.spsummonFilter, tp, LOCATION_MZONE, 0, 2, 2, nil)
     Duel.SendtoDeck(g, nil, 2, REASON_COST)
-    Duel.SpecialSummon(e:GetHandler(), SUMMON_TYPE_SPECIAL, tp, tp, true, true, POS_FACEUP_ATTACK)
+    Duel.ShuffleDeck(tp)
 end
 
 function c284130824.toHandTarget(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
@@ -93,9 +81,9 @@ function c284130824.toHandTarget(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
         return Duel.IsExistingMatchingCard(Card.IsSetCard, tp, LOCATION_GRAVE, 0, 1, nil, 0x2222)
     end
     local g = Duel.SelectMatchingCard(tp, Card.IsSetCard, tp, LOCATION_GRAVE, 0, 1, 1, nil, 0x2222)
-    -- 因为该效果要求不能被连锁，所以取对象不入连锁
 
     Duel.SetOperationInfo(0, CATEGORY_TOHAND, g, 1, nil, 0)
+    -- 因为该效果要求不能被连锁，所以取对象不入连锁
 end
 
 function c284130824.toHandOperation(e, tp, eg, ep, ev, re, r, rp)
