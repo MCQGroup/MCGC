db74f7fbdaf711c5ee5fc8e9c9cbab601474f10c
SLAPaper <slapaper.pku@gmail.com>
2015-07-21 23:58:22 +0800

    add e1

--- MCGC/c284130832.lua
+++ MCGC/c284130832.lua
@@ -2,6 +2,12 @@
 
 function c284130832.initial_effect(c)
     -- 当作魔法陷阱
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_MONSTER_SSET)
+    e1:SetValue(TYPE_SPELL)
+    c:RegisterEffect(e1)
+
     -- 破坏触发特招
     -- 反转触发
 end
