9189605d4f469140b945a57a3651e94cb1f93a65
SLAPaper <slapaper.pku@gmail.com>
2015-07-31 23:48:49 +0800

    doing with

--- MCGC/c284130833.lua
+++ MCGC/c284130833.lua
@@ -1,5 +1,20 @@
 -- MC群的战士 节操
 
 function c284130833.initial_effect(c)
+    -- 一回合只使用一次
+    -- 召唤触发抉择
+    local e1 = Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TODECK)
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetTarget(c284130833.target)
+    e1:SetOperation(c284130833.operation)
+    e1:SetCountLimit(1, 284130833)
+    c:RegisterEffect(e1)
+end
 
+function c284130833.target(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+end
+
+function c284130833.operation(e, tp, eg, ep, ev, re, r, rp)
 end
