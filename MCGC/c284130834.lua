0cf1803648ed4928c8365863b14b11e900d517d1
SLAPaper <slapaper.pku@gmail.com>
2015-08-04 23:36:59 +0800

    doing with c284130834

--- MCGC/c284130834.lua
+++ MCGC/c284130834.lua
@@ -25,12 +25,19 @@ function c284130834.operation(e, tp, eg, ep, ev, re, r, rp)
     local g = Duel.GetChainInfo(0, CHAININFO_TARGET_CARDS)
     local c1 = g:GetFirst()
     local e1 = Effect.CreateEffect(c1)
-    -- here set the delay trigger
+    e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    e1:SetCode(EVENT_PHASE + PHASE_STANDBY)
+    -- 第三个回合发动
+    e1:SetCondition()
+    e1:SetOperation()
     c1:RegisterEffect(e1)
     Duel.Remove(c1, POS_FACEUP, REASON_EFFECT)
 
     local c2 = e:GetHandler()
     local e2 = Effect.CreateEffect(c2)
     -- here set the battle immune
+    e2;SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    -- 第三个回合重置
     c2:RegisterEffect(e2)
 end
