9c95e4e9fb288783beeb11276c39ee1749cb9710
SLAPaper <slapaper.pku@gmail.com>
2015-08-06 01:25:00 +0800

    finished c2841380834 (not tested)

--- MCGC/c284130834.lua
+++ MCGC/c284130834.lua
@@ -23,22 +23,52 @@ end
 
 function c284130834.operation(e, tp, eg, ep, ev, re, r, rp)
     local g = Duel.GetChainInfo(0, CHAININFO_TARGET_CARDS)
+
+    -- 除外并且三回合回手
     local c1 = g:GetFirst()
     local e1 = Effect.CreateEffect(c1)
     e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
     e1:SetCode(EVENT_PHASE + PHASE_STANDBY)
-    -- 第三个回合发动
-    e1:SetCondition()
-    e1:SetOperation()
+    e1:SetLabel(0)
+    e1:SetCondition(c284130834.thirdTriggerCondition)
+    e1:SetOperation(c284138034.thirdTriggerOperation)
     c1:RegisterEffect(e1)
     Duel.Remove(c1, POS_FACEUP, REASON_EFFECT)
-
+    
+    -- 战破免疫三回合
     local c2 = e:GetHandler()
     local e2 = Effect.CreateEffect(c2)
-    -- here set the battle immune
-    e2;SetType(EFFECT_TYPE_SINGLE)
+    e2:SetType(EFFECT_TYPE_SINGLE)
     e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-    e2:SetReset()
-    -- 第三个回合重置
+    e2:SetReset(RESET_STANDBY + RESET_SELF_TURN, 3)
     c2:RegisterEffect(e2)
+
+    -- 送墓触发
+    local e3 = Effect.CreateEffect(c2)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+    e3:SetCode(EVENT_TO_GRAVE)
+    e3:SetReset(RESET_STANDBY + RESET_SELF_TURN, 3)
+    e3:SetLabelObject(e1)
+    e3:SetOperation(c284138034.toGraveTriggerOperation)
+    c2:RegisterEffect(e3)
+end
+
+function c284130834.thirdTriggerCondition(e, tp, eg, ep, ev, re, r, rp)
+    return Duel.GetTurnPlayer() == tp
+end
+
+function c284130834.thirdTriggerOperation(e, tp, eg, ep, ev, re, r, rp)
+    local count = e:GetLabel()
+    if count == 3 then
+        Duel.SendtoHand(e:GetHandler(), tp, REASON_EFFECT)
+        e:Reset()
+    else
+        e:SetLabel(count + 1)
+    end
+end
+
+function c284138034.toGraveTriggerOperation(e, tp, eg, ep, ev, re, r, rp)
+    local e = e3:GetLabelObject()
+    Duel.SendtoDeck(e:GetHandler(), tp, REASON_EFFECT)
+    e:Reset()
 end
