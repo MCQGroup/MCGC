7c4e400ec8e0a6bdbb609c19feb87af4fc913621
SLAPaper <slapaper.pku@gmail.com>
2015-08-14 22:23:19 +0800

    debuging 木头

--- MCGC/c284130831.lua
+++ MCGC/c284130831.lua
@@ -48,9 +48,9 @@ function c284130831.removeForSpSummonOperation(e, tp, eg, ep, ev, re, r, rp)
 
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
-    e1:SetRange(LOCATION_REMOVED)
     e1:SetCode(EVENT_PHASE + PHASE_STANDBY)
-    e1:SetCondition(c284130831.delayTriggerCondition)
+    e1:SetRange(LOCATION_REMOVED)
+--    e1:SetCondition(c284130831.delayTriggerCondition)
     e1:SetOperation(c284130831.delayTriggerOperation)
     c:RegisterEffect(e1)
 
