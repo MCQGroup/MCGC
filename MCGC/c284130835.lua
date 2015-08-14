1eaddccd9eeedce50d1e66fd9ae54b79c37726fb
SLAPaper <slapaper.pku@gmail.com>
2015-08-14 23:52:12 +0800

    finished 要触 (not tested)

--- MCGC/c284130835.lua
+++ MCGC/c284130835.lua
@@ -8,21 +8,60 @@ function c284130835.initial_effect(c)
     e1:SetCode(EFFECT_SPSUMMON_CONDITION)
     c:RegisterEffect(e1)
 
-    -- 蜘蛛
-    -- 攻击力上升场上的卡的数量
+    -- 昆虫
     local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetCode(EFFECT_UPDATE_ATTACK)
-    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetValue(c284130835.updateAtk)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetCode(EFFECT_FORBIDDEN)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CANNOT_DISABLE)
+    e2:SetTarget(c284130835.banTarget)
     c:RegisterEffect(e2)
 
-    -- 破坏送墓触发
-    local e3 = Effect.CreateEffect(c)
+    local e3 = e2:Clone()
+    e3:SetCode(EFFECT_DISABLE)
     c:RegisterEffect(e3)
+
+    -- 攻击力上升场上的卡的数量
+    local e4 = Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE)
+    e4:SetCode(EFFECT_UPDATE_ATTACK)
+    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetValue(c284130835.updateAtk)
+    c:RegisterEffect(e4)
+
+    -- 破坏送墓触发
+    local e5 = Effect.CreateEffect(c)
+    e5:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e5:SetCode(EVENT_DESTROY)
+    e5:SetTarget(c284130835.destroyTriggerTarget)
+    e5:SetOperation(c284130835.destroyTriggerOperation)
+    c:RegisterEffect(e5)
+end
+
+function c284130835.banFilter(c)
+    return c:IsRace(RACE_INSECT) and c:IsFaceup()
+end
+
+function c284130835.banTarget(e, c)
+    return Duel.IsExistingMatchingCard(c284130835.banFilter, tp, LOCATION_ONFIELD, LOCATION_ONFIELD, 1, nil)
 end
 
 function c284130835.updateAtk(e, c)
     return Duel.GetFieldGroupCount(PLAYER_ALL, LOCATION_ONFIELD, LOCATION_ONFIELD)
+end
+
+function c284130835.destroyTriggerTarget(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chkc then
+        return chkc:IsDestructable()
+    end
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(Card.IsDestructable, tp, LOCATION_ONFIELD, LOCATION_ONFIELD, 1, nil)
+    end
+    local g = Duel.SelectTarget(tp, Card.IsDestructable, tp, LOCATION_ONFIELD, LOCATION_ONFIELD, 1, 1, nil)
+    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, 1, nil, 0)
+end
+
+function c284130835.destroyTriggerOperation(e, tp, eg, ep, ev, re, r, rp)
+    local g = Duel.GetChainInfo(0, CHAININFO_TARGET_CARDS)
+    Duel.Destroy(g, REASON_EFFECT, LOCATION_GRAVE)
 end
