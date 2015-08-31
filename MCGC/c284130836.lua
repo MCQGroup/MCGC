f6a85d371f888795d8062994cd7c66a9973a671d
SLAPaper <slapaper.pku@gmail.com>
2015-08-31 09:44:53 +0800

    revert the c284130836

--- MCGC/c284130836.lua
+++ MCGC/c284130836.lua
@@ -35,13 +35,13 @@ function c284130836.spop(e, tp, eg, ep, ev, re, r, rp)
         local e1 = Effect.CreateEffect(tc)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_DISABLE)
---        e1:SetReset(RESET_EVENT + 0x1FE0000)
+        e1:SetReset(RESET_EVENT + 0x1FE0000)
         -- 0x01FE0000 = RESET_TOFIELD + RESET_LEAVE + RESET_TODECK + RESET_TOHAND + RESET_TEMP_REMOVE + RESET_REMOVE + RESET_TOGRAVE + RESET_TURN_SET
         tc:RegisterEffect(e1)
 
         local e2 = e1:Clone()
         e2:SetCode(EFFECT_DISABLE_EFFECT)
---        e2:SetValue(RESET_TURN_SET)
+        e2:SetValue(RESET_TURN_SET)
         tc:RegisterEffect(e2)
     end
     Duel.SpecialSummonComplete()
