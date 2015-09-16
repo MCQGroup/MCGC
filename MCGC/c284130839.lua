c606e7969f1b282a06e9f4a98d45b15d6ab65da2
SLAPaper <slapaper.pku@gmail.com>
2015-09-16 22:42:37 +0800

    update ygoLib and c284130839

--- MCGC/c284130839.lua
+++ MCGC/c284130839.lua
@@ -2,8 +2,15 @@
 
 function c284130839.initial_effect(c)
 	-- 同调召唤
+	c:EnableReviveLimit()
+	aux.AddSynchroProcedure(c,c284130839.filter,c284130839.filter,1)
+	
 	-- 同调召唤成功触发
 	-- 战破触发
 	-- 结束阶段必定发动
 	-- 因效果破坏必定发动
 end	
+
+function c284130839.filter(c)
+  return c:IsSetCard(0x2222)
+end
