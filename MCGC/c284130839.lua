8198e21fa42c86947ca6af67ae55775d136d679f
SLAPaper <slapaper.pku@gmail.com>
2015-09-18 21:07:03 +0800

    update c284130839

--- MCGC/c284130839.lua
+++ MCGC/c284130839.lua
@@ -1,16 +1,31 @@
 -- MC群服务器OP 无情
 
 function c284130839.initial_effect(c)
-    -- 同调召唤
-    c:EnableReviveLimit()
-    aux.AddSynchroProcedure(c, c284130839.filter, c284130839.filter, 1)
+	-- 同调召唤
+	c:EnableReviveLimit()
+	aux.AddSynchroProcedure(c, c284130839.filter, c284130839.filter, 1)
 
-    -- 同调召唤成功触发
-    -- 战破触发
-    -- 结束阶段必定发动
-    -- 因效果破坏必定发动
-end	
+	-- 同调召唤成功触发
+	local e1 = Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(c284130839.syncSummonSuccessTriggerCondition)
+	e1:SetOperation(c284130839.syncSummonSuccessTriggerOperation)
+	c:RegisterEffect(e1)
+	
+	-- 战破触发
+	-- 结束阶段必定发动
+	-- 因效果破坏必定发动
+end 
 
 function c284130839.filter(c)
-    return c:IsSetCard(0x2222)
+	return c:IsSetCard(0x2222)
+end
+
+function c284130839.syncSummonSuccessTriggerCondition(e, tp, eg, ep, ev, re, r, rp)
+	return bit.band(e:GetHandler():GetSummonType(), SUMMON_TYPE_SYNCHRO) == SUMMON_TYPE_SYNCHRO
+end
+
+function c284130839.syncSummonSuccessTriggerOperation(e, tp, eg, ep, ev, re, r, rp)
+	-- 这个效果是对场地生效，还是对当前场上所有的怪物生效？
 end
