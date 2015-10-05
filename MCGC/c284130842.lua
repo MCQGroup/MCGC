9fda3ad342bd38c6f1d10fb8c6deb5f31861ce85
SLAPaper <slapaper.pku@gmail.com>
2015-10-06 00:27:11 +0800

    update c284130842

--- MCGC/c284130842.lua
+++ MCGC/c284130842.lua
@@ -9,14 +9,22 @@ function c284130842.initial_effect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
     e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetRange(LOCATION_EXTRA)
     c:RegisterEffect(e1)
 	
 	-- 不被效果破坏
+	-- 参考[94977269]神影依·米德拉什
 	local e2 = Effect.CreateEffect(c)
-	
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetValue(c284130842.indestructableValue)
+	c:RegisterEffect(e2)
 	
 	-- 攻击上升
-	
+	local e3 = Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode()
 	
 	-- 防御穿透
 		
@@ -28,4 +36,8 @@ end
 
 function c284130842.fusionFilter2(c)
 	return c:IsAttribute(ATTRIBUTE_FIRE)
+end
+
+function c284130842.indestructableValue(e, re, tp)
+	return tp ~= e:GetHandlerPlayer()
 end
