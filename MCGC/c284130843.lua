effb110a255fd18a27a4e43022220dea4651a159
SLAPaper <slapaper.pku@gmail.com>
2015-10-15 23:06:17 +0800

    check in

--- MCGC/c284130843.lua
+++ MCGC/c284130843.lua
@@ -4,14 +4,26 @@ function c284130843.intial_effect(c)
 	-- 同调召唤
 	c:EnableReviveLimit()
 	aux.AddSynchroProcedure(c, c284130843.synchroFilter1, c284130843.synchroFilter2, 2)
-	local e1 = Effect.CreateEffect(c)
-	c:RegisterEffect(e1)
+	
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+    c:RegisterEffect(e1)
 	
 	-- 展示
 	local e2 = Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_IGNITION)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_EXTRA)
+	e2:SetCondition(c284130843.showCondition)
+	e2:SetCost(c284130843.showCost)
+	e2:SetOperation(c284130843.showOperation)
 	c:RegisterEffect(e2) 
 	
 	-- 魔免
+	
+	
 	-- 控制权
 	-- 无效效果
 	-- 亡语
@@ -27,4 +39,19 @@ end
 
 function c284130843.synchroFilter2(c)
 	return (c:IsType(TYPE_FUSION) or c:IsType(TYPE_XYZ) or c:IsType(TYPE_PENDULUM))
+end
+
+function c284130843.showCondition(e, tp, eg, ep, ev, re, r, rp)
+	return Duel.GetTurnPlayer() == tp and bit.band(Duel.GetCurrentPhase(), PHASE_MAIN1 + PHASE_MAIN2)
+end
+
+function c284130843.showCost(e, tp, eg, ep, ev, re, r, rp, chk)
+	if chk == 0 then
+		return true
+	end
+	Duel.ConfirmCards(1 - tp, e:GetHandler())
+end
+
+function c284130843.showOperation(e, tp, eg, ep, ev, re, r, rp)
+	-- 这尼玛怎么写！
 end
