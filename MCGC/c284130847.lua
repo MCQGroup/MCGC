7f94e30749b46e9db34a1ad50c4bc895ed74d639
SLAPaper <slapaper.pku@gmail.com>
2015-11-13 00:44:59 +0800

    Processing c284130847

--- MCGC/c284130847.lua
+++ MCGC/c284130847.lua
@@ -5,9 +5,23 @@
 -- MC群服务器失踪者 TF
 function c284130847.initial_effect(c)
     -- 融合
+    c:EnableReviveLimit()
+    aux.AddFusionProcFun2(c, c284130847.fusionFilter1, c284130847.fusionFilter2, false)
 
     -- 对象触发
 
 end 
+
+function c284130847.filter(c)
+    return c:IsSetCard(0x2222)
+end
+
+function c284130847.fusionFilter1(c)
+    return c284130847.filter(c) and c:IsType(TYPE_MONSTER)
+end
+
+function c284130847.fusionFilter2(c)
+    return c:IsAttribute(ATTRIBUTE_DARK) and c:IsType(TYPE_MONSTER)
+end
 
 -- endregion
