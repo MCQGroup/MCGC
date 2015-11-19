ff3496378c5fae900f0d8b29b8b7ad2d41b26801
SLAPaper <slapaper.pku@gmail.com>
2015-11-19 18:08:46 +0800

    Updated c284130848

--- MCGC/c284130848.lua
+++ MCGC/c284130848.lua
@@ -9,8 +9,21 @@ function c284130848.initial_effect(c)
     aux.AddXyzProcedure(c, aux.FilterBoolFunction(Card.IsSetCard, 0x2222), 6, 4, c284130848.ovfilter, aux.Stringid(284130848, 0))
 
     -- 超量成功触发
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetCondition()
+    e1:SetOperation()
+    c:RegisterEffect(e1)
 
     -- 一回合一次
+    local e2 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCondition()
+    e1:SetCost()
+    e1:SetOperation()
+    c:RegisterEffect(e2)
 
 end
 
