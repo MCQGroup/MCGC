a7759878eefbfb1da1b6926593b6f12422236646
SLAPaper <slapaper.pku@gmail.com>
2015-12-02 23:58:18 +0800

    Check in

--- MCGC/c284130850.lua
+++ MCGC/c284130850.lua
@@ -6,7 +6,14 @@
 function c284130850.initial_effect(c)
     -- 发动
     local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
     c:RegisterEffect(e1)
+
+    -- 墓地起动
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_GRAVE)
+    c:RegisterEffect(e2)
 end
 
 --endregion
