02cd0d389b976d9475054a88e0b8c1cf381d6d53
SLAPaper <slapaper.pku@gmail.com>
2015-12-05 22:20:52 +0800

    updated c284130850

--- MCGC/c284130850.lua
+++ MCGC/c284130850.lua
@@ -7,21 +7,43 @@ function c284130850.initial_effect(c)
     -- 发动
     -- 参考[65331686]毛绒动物·猫头鹰
     local e1 = Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCondition()
-    e1:SetTarget()
-    e1:SetOperation()
+    e1:SetCost(c284130850.activateCost)
+    e1:SetTarget(c284130850.activateTarget)
+    e1:SetOperation(c284130850.activateOperation)
     c:RegisterEffect(e1)
 
     -- 墓地起动
     local e2 = Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetRange(LOCATION_GRAVE)
-    e2:SetCost()
-    e2:SetCondition()
-    e2:SetTarget()
-    e2:SetOperation()
+    e2:SetCondition(c284130850.graveCondition)
+    e2:SetCost(c284130850.graveCost)
+    e2:SetOperation(c284130850.graveOperation)
     c:RegisterEffect(e2)
+end
+
+function c284130850.filter(c)
+    return c:IsSetCard(0x2222)
+end
+
+function c284130850.activateCost(e, tp, eg, ep, ev, re, r, rp, chk)
+end
+
+function c284130850.activateTarget(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+end
+
+function c284130850.activateOperation(e, tp, eg, ep, ev, re, r, rp)
+end
+
+function c284130850.graveCondition(e, tp, eg, ep, ev, re, r, rp)
+end
+
+function c284130850.graveCost(e, tp, eg, ep, ev, re, r, rp, chk)
+end
+
+function c284130850.graveOperation(e, tp, eg, ep, ev, re, r, rp)
 end
 
 --endregion
