6408a8c47cb9a70b7057eef7ccfe7ead40dc15a0
SLAPaper <slapaper.pku@gmail.com>
2015-12-10 22:56:43 +0800

    check in

--- MCGC/c284130850.lua
+++ MCGC/c284130850.lua
@@ -1,6 +1,6 @@
---region *.lua
---2015-11-26
---此文件由[BabeLua]插件自动生成
+-- region *.lua
+-- 2015-11-26
+-- 此文件由[BabeLua]插件自动生成
 
 -- MC群融合
 function c284130850.initial_effect(c)
@@ -9,7 +9,6 @@ function c284130850.initial_effect(c)
     local e1 = Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCost(c284130850.activateCost)
     e1:SetTarget(c284130850.activateTarget)
     e1:SetOperation(c284130850.activateOperation)
     c:RegisterEffect(e1)
@@ -28,16 +27,16 @@ function c284130850.filter(c)
     return c:IsSetCard(0x2222)
 end
 
-function c284130850.activateCost(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then
-        return
-    end
+function c284130850.filter2(c, e)
+    return c:IsCanBeFusionMaterial() and not c:IsImmuneToEffect(e)
+end
+
+function c284130850.filter3(c, e, tp, m, f, chkf)
+    return c:IsType(TYPE_FUSION) and c:IsSetCard(0xad) and(not f or f(c))
+    and c:IsCanBeSpecialSummoned(e, SUMMON_TYPE_FUSION, tp, false, false) and c:CheckFusionMaterial(m, nil, chkf)
 end
 
 function c284130850.activateTarget(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
-    if chkc then
-        return
-    end
     if chk == 0 then
         return
     end
@@ -47,7 +46,7 @@ function c284130850.activateOperation(e, tp, eg, ep, ev, re, r, rp)
 end
 
 function c284130850.graveCondition(e, tp, eg, ep, ev, re, r, rp)
-    return 
+    return
 end
 
 function c284130850.graveCost(e, tp, eg, ep, ev, re, r, rp, chk)
@@ -59,4 +58,4 @@ end
 function c284130850.graveOperation(e, tp, eg, ep, ev, re, r, rp)
 end
 
---endregion
+-- endregion
