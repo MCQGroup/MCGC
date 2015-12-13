39e65b13f5366923f3dbf456760c2d9d8940ea80
SLAPaper <slapaper.pku@gmail.com>
2015-12-13 23:03:36 +0800

    check in

--- MCGC/c284130850.lua
+++ MCGC/c284130850.lua
@@ -52,10 +52,50 @@ function c284130850.activateTarget(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
         end
         return res
     end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_EXTRA)
 end
 
 function c284130850.activateOperation(e, tp, eg, ep, ev, re, r, rp)
+    local chkf = Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and PLAYER_NONE or tp
+    local mg1 = Duel.GetMatchingGroup(c284130850.filter2, tp, LOCATION_HAND + LOCATION_MZONE, 0, nil, e)
+    local sg1 = Duel.GetMatchingGroup(c284130850.filter3, tp, LOCATION_EXTRA, 0, nil, e, tp, mg1, nil, chkf)
+    local mg2 = nil
+    local sg2 = nil
+    local ce = Duel.GetChainMaterial(tp)
+    if ce ~= nil then
+        local fgroup = ce:GetTarget()
+        mg2 = fgroup(ce, e, tp)
+        local mf = ce:GetValue()
+        sg2 = Duel.GetMatchingGroup(c284130850.filter3, tp, LOCATION_EXTRA, 0, nil, e, tp, mg2, mf, chkf)
+    end
+    if sg1:GetCount() > 0 or(sg2 ~= nil and sg2:GetCount() > 0) then
+        local sg = sg1:Clone()
+        if sg2 then sg:Merge(sg2) end
+        Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+        local tg = sg:Select(tp, 1, 1, nil)
+        local tc = tg:GetFirst()
+        if sg1:IsContains(tc) and(sg2 == nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp, ce:GetDescription())) then
+            local mat1 = Duel.SelectFusionMaterial(tp, tc, mg1, nil, chkf)
+            tc:SetMaterial(mat1)
+            Duel.SendtoGrave(mat1, REASON_EFFECT + REASON_MATERIAL + REASON_FUSION)
+            Duel.BreakEffect()
+            Duel.SpecialSummon(tc, SUMMON_TYPE_FUSION, tp, tp, false, false, POS_FACEUP)
+        else
+            local mat2 = Duel.SelectFusionMaterial(tp, tc, mg2, nil, chkf)
+            local fop = ce:GetOperation()
+            fop(ce, e, tp, tc, mat2)
+        end
+        tc:CompleteProcedure()
+    else
+        local cg1 = Duel.GetFieldGroup(tp, LOCATION_HAND + LOCATION_MZONE, 0)
+        local cg2 = Duel.GetFieldGroup(tp, LOCATION_EXTRA, 0)
+        if cg1:GetCount() > 1 and cg2:IsExists(Card.IsFacedown, 1, nil)
+            and Duel.IsPlayerCanSpecialSummon(tp) and not Duel.IsPlayerAffectedByEffect(tp, 27581098) then
+            Duel.ConfirmCards(1 - tp, cg1)
+            Duel.ConfirmCards(1 - tp, cg2)
+            Duel.ShuffleHand(tp)
+        end
+    end
 end
 
 function c284130850.graveCondition(e, tp, eg, ep, ev, re, r, rp)
