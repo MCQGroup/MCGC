426aec6d27ac7702b9115692ad3d1e9096de9e6f
SLAPaper <slapaper.pku@gmail.com>
2015-12-21 23:47:27 +0800

    check in

--- MCGC/c284130851.lua
+++ MCGC/c284130851.lua
@@ -2,5 +2,15 @@
 
 function c284130851.initial_effect(c)
     local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetOperation()
     c:RegisterEffect(e1)
+end
+
+function c284130851.filter(c)
+    return c:IsSetCard(0x2222)
+end
+
+function c284130851.operation(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
 end
