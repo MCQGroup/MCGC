9657b9e4179043efa06c14580c540c25aab9d0e9
SLAPaper <slapaper.pku@gmail.com>
2015-12-22 23:54:34 +0800

    Check in

--- MCGC/c284130851.lua
+++ MCGC/c284130851.lua
@@ -3,7 +3,7 @@
 function c284130851.initial_effect(c)
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetOperation()
+    e1:SetOperation(c284130851.operation)
     c:RegisterEffect(e1)
 end
 
@@ -11,6 +11,26 @@ function c284130851.filter(c)
     return c:IsSetCard(0x2222)
 end
 
+function c284130851.desdroyFilter(c)
+    return c284130851.filter(c) and c:IsType(TYPE_MONSTER) and c:IsDestructable()
+end
+
 function c284130851.operation(e, tp, eg, ep, ev, re, r, rp)
+    local g = Duel.GetMatchingGroup(c284130851.desdroyFilter, tp, LOCATION_ONFIELD, 0, nil)
+    local count = g:GetCount()
+    Duel.Destroy(g, REASON_EFFECT, LOCATION_GRAVE)
+
     local c = e:GetHandler()
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_CONTINUOUS)
+    e1:SetCondition(EVENT_PHASE + PHASE_STANDBY)
+    e1:SetCondition(c284130851.delayCondition)
+    e1:SetOperation(c284130851.delayOperation)
+end
+
+function c284130851.delayCondition(e, tp, eg, ep, ev, re, r, rp)
+    
+end
+
+function c284130851.delayOperation(e, tp, eg, ep, ev, re, r, rp)
 end
