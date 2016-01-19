d126aa5c5595d1994cdc98cbb5ad3f4ced5c3d20
SLAPaper <slapaper.pku@gmail.com>
2016-01-19 17:29:47 +0800

    fix (c84130846): make it work

--- MCGC/c84130846.lua
+++ MCGC/c84130846.lua
@@ -16,10 +16,13 @@ function c84130846.initial_effect(c)
     -- 效果代破
     -- 参考[78156759]发条机雷 发条雷
     local e2 = Effect.CreateEffect(c)
+    local e4 = Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(84130846, 1))
     e2:SetType(EFFECT_TYPE_CONTINUOUS + EFFECT_TYPE_SINGLE)
     e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
     e2:SetCode(EFFECT_DESTROY_REPLACE)
     e2:SetRange(LOCATION_MZONE)
+    e2:SetLabelObject(e4)
     e2:SetTarget(c84130846.replaceTarget)
     c:RegisterEffect(e2)
 
@@ -31,14 +34,15 @@ function c84130846.initial_effect(c)
     e3:SetCountLimit(1)
     e3:SetCondition(c84130846.searchCondition)
     e3:SetTarget(c84130846.searchTarget)
-    e3:SetOperation(c84130811.searchOperation)
+    e3:SetOperation(c84130846.searchOperation)
     c:RegisterEffect(e3)
 
     -- 维持代价
-    local e4 = Effect.CreateEffect(c)
-    e4:SetType(EFFECT_TYPE_CONTINUOUS)
+    e4:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
     e4:SetCode(EVENT_PHASE + PHASE_STANDBY)
     e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetCondition(c84130846.keepCondition)
     e4:SetOperation(c84130846.keepOperation)
     c:RegisterEffect(e4)
 end
@@ -55,7 +59,7 @@ function c84130846.replaceTarget(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     if chk == 0 then
         return e:GetHandler():CheckRemoveOverlayCard(tp, 1, REASON_EFFECT)
     end
-    if Duel.SelectYesNo(tp, aux.Stringid(84130846, 1)) then
+    if re ~= e:GetLabelObject() and Duel.SelectYesNo(tp, aux.Stringid(84130846, 1)) then
         e:GetHandler():RemoveOverlayCard(tp, 1, 1, REASON_EFFECT)
         e:GetHandler():RegisterFlagEffect(84130846, RESET_EVENT + 0x1fe0000 + RESET_PHASE + PHASE_END, 0, 1)
         return true
@@ -69,7 +73,7 @@ function c84130846.searchFilter(c)
 end
 
 function c84130846.searchCondition(e, tp, eg, ep, ev, re, r, rp)
-    return e:GetHandler():GetFlagEffect(78156759) ~= 0
+    return e:GetHandler():GetFlagEffect(84130846) ~= 0
 end
 
 function c84130846.searchTarget(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
@@ -77,7 +81,7 @@ function c84130846.searchTarget(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
         return Duel.IsExistingMatchingCard(c84130846.searchFilter, tp, LOCATION_DECK + LOCATION_GRAVE, 1, 1, nil)
     end
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
-    local g = Duel.SelectMatchingCard(tp, c84130846.searchFilter, tp, LOCATION_DECK + LOCATION_GRAVE, 1, 1, nil)
+    local g = Duel.SelectMatchingCard(tp, c84130846.searchFilter, tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1, nil)
     Duel.SetOperationInfo(0, CATEGORY_SEARCH, g, 1, nil, nil)
 end
 
@@ -86,6 +90,10 @@ function c84130846.searchOperation(e, tp, eg, ep, ev, re, r, rp)
     if test then
         Duel.SendtoHand(g, tp, REASON_EFFECT)
     end
+end
+
+function c84130846.keepCondition(e, tp, eg, ep, ev, re, r, rp)
+    return Duel.GetTurnPlayer() == tp
 end
 
 function c84130846.keepOperation(e, tp, eg, ep, ev, re, r, rp)
@@ -97,5 +105,5 @@ function c84130846.keepOperation(e, tp, eg, ep, ev, re, r, rp)
             return
         end
     end
-    Duel.Destroy(c, REASON_EFFECT, LOCATION_GRAVE)
+    Duel.Destroy(c, REASON_RULE, LOCATION_GRAVE)
 end
