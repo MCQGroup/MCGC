ecbcac3776c2f022046c4aa22c36d1c9cf82dbca
SLAPaper <slapaper.pku@gmail.com>
2016-02-20 14:21:20 +0800

    feature (c84130852): finish c84130852

--- MCGC/c84130852.lua
+++ MCGC/c84130852.lua
@@ -6,7 +6,8 @@
 function c84130852.initial_effect(c)
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetOperation()
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetOperation(c84130852.operation)
     c:RegisterEffect(e1)
 end
 
@@ -23,10 +24,10 @@ function c84130852.operation(e, tp, eg, ep, ev, re, r, rp)
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_CONTINUOUS)
     e1:SetLabel(count)
-    e1:SetCondition(EVENT_PHASE + PHASE_STANDBY)
+    e1:SetCode(EVENT_PHASE + PHASE_STANDBY)
     e1:SetCondition(c84130852.delayCondition)
     e1:SetOperation(c84130852.delayOperation)
-    c:RegisterEffect(e1)
+    Duel.RegisterEffect(e1, tp)
 end
 
 function c84130852.delayCondition(e, tp, eg, ep, ev, re, r, rp)
@@ -36,13 +37,25 @@ end
 function c84130852.delayOperation(e, tp, eg, ep, ev, re, r, rp)
     local count = e:GetLabel()
     local g = Duel.SelectMatchingCard(tp, Card.IsType, tp, LOCATION_GRAVE, 0, count, count, nil, TYPE_SPELL + TYPE_TRAP)
-    g:ForEach( function(c)
+    local c = g:GetFirst()
+    while c do
         Duel.MoveToField(c, tp, tp, LOCATION_SZONE, POS_FACEDOWN, true)
-        local e1 = Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_CANNOT_ACTIVATE)
-        e1:SetReset(RESET_PHASE + PHASE_END + RESET_OPPO_TURN)
-        c:RegisterEffect(e1)
+        c = g:GetNext()
+    end
+    g:KeepAlive()
+
+    local e1 = Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+    e1:SetTargetRange(1, 1)
+    e1:SetLabelObject(g)
+    e1:SetValue( function(e, re, tp)
+        return e:GetLabelObject():IsContains(re:GetHandler()) and not re:GetHandler():IsImmuneToEffect(e)
     end )
+    e1:SetReset(RESET_PHASE + PHASE_END + RESET_OPPO_TURN)
+    Duel.RegisterEffect(e1, tp)
+
+    e:Reset()
 end
 -- endregion
