9c3a3822d84078a92eac32f6a16b816a4b50b654
SLAPaper <slapaper.pku@gmail.com>
2016-03-12 13:50:58 +0800

    refactor (c84130859): processing

--- MCGC/c84130859.lua
+++ MCGC/c84130859.lua
@@ -1,6 +1,6 @@
---region *.lua
---Date 2016-03-09
---此文件由[BabeLua]插件自动生成
+-- region *.lua
+-- Date 2016-03-09
+-- 此文件由[BabeLua]插件自动生成
 
 -- DZ的表情本体
 function c84130859.initial_effect(c)
@@ -20,11 +20,20 @@ function c84130859.initial_effect(c)
 end
 
 function c84130859.DZFilter(c)
-    return c:IsCode() or c:IsCode() or c:IsCode()
+    return c:IsCode(84130813) or c:IsCode(84130840)
+end
+
+function c84130859.spsummonFilter(c, e, sumtype, sumplayer, nocheck, nolimit, sumpos, target_player)
+    return c84130859.DZFilter(c) and c:IsCanBeSpecialSummoned(e, sumtype, sumplayer, nocheck, nolimit, sumpos, target_player)
 end
 
 function c84130859.operation(e, tp, eg, ep, ev, re, r, rp)
     Duel.Hint(HINT_MESSAGE, tp, HINTMSG_SPSUMMON)
-    local g = Duel.SelectMatchingCard(tp, c84130859.DZFilter, tp, LOCATION_DECK, 0, 1, 1, nil)
+    local g = Duel.SelectMatchingCard(tp, c84130859.spsummonFilter, tp, LOCATION_DECK, 0, 1, 1, nil, e, SUMMON_TYPE_SPECIAL, tp, false, false, POS_FACEUP, tp)
+    if g:GetCount() > 0 then
+        local c = g:GetFirst()
+        local pos = Duel.SelectPosition(tp, c, POS_FACEUP)
+        Duel.SpecialSummon(c, SUMMON_TYPE_SPECIAL, tp, tp, false, false, pos)
+    end
 end
---endregion
+-- endregion
