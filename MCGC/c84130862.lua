74719ac536197a552e1eaa7d30cb5c6e7e035be6
SLAPaper <slapaper.pku@gmail.com>
2016-03-21 20:54:44 +0800

    refactor (c84130862): processing

--- MCGC/c84130862.lua
+++ MCGC/c84130862.lua
@@ -4,7 +4,23 @@
 
 -- MC群暴动
 function c84130862.initial_effect(c)
-    
+    -- 发动
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetOperation()
+    c:RegisterEffect(e1)
+
+    -- 墓地除外
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCondition()
+    e2:SetCost()
+    e2:SetTarget()
+    e2:SetOperation()
+    c:RegisterEffect(e2)
 end
 
 --endregion
