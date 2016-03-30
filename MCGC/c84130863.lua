2c25144f9a93aa8b8995a8a204235747cac19536
SLAPaper <slapaper.pku@gmail.com>
2016-03-30 23:28:23 +0800

    refactor (c84130863): check in

--- MCGC/c84130863.lua
+++ MCGC/c84130863.lua
@@ -3,6 +3,12 @@
 --此文件由[BabeLua]插件自动生成
 
 function c84130863.initial_effect(c)
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_ACTIVATE);
+    e1:SetCode(EVENT_FREE_CHAIN);
+    e1:SetTarget()
+    e1:SetOperation()
+    c:RegisterEffect(e1)
 end
 
 --endregion
