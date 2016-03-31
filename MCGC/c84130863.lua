451135ba7a7e04ba1a6ba0923856ddf35e3fd020
SLAPaper <slapaper.pku@gmail.com>
2016-03-31 23:53:51 +0800

    refactor (c84130863): check in

--- MCGC/c84130863.lua
+++ MCGC/c84130863.lua
@@ -6,7 +6,7 @@ function c84130863.initial_effect(c)
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE);
     e1:SetCode(EVENT_FREE_CHAIN);
-    e1:SetTarget()
+    e1:SetTarget(c84130863.target)
     e1:SetOperation()
     c:RegisterEffect(e1)
 end
