5485b93f3eaaf3a9d32e4c6d9de818198714e7c5
SLAPaper <slapaper.pku@gmail.com>
2016-03-31 23:53:18 +0800

    refactor (c84130863): check in

--- MCGC/c84130863.lua
+++ MCGC/c84130863.lua
@@ -1,6 +1,6 @@
---region *.lua
---Date 2016-03-29
---此文件由[BabeLua]插件自动生成
+-- region *.lua
+-- Date 2016-03-29
+-- 此文件由[BabeLua]插件自动生成
 
 function c84130863.initial_effect(c)
     local e1 = Effect.CreateEffect(c)
@@ -11,4 +11,16 @@ function c84130863.initial_effect(c)
     c:RegisterEffect(e1)
 end
 
---endregion
+function c84130863.spFilter(c, e, sumtype, sumplayer, nocheck, nolimit, sumpos, target_player)
+    return c:IsSetCard(0x2222) and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e, sumtype, sumplayer, nocheck, nolimit, sumpos, target_player)
+end
+
+function c84130863.target(e, tp, eg, ep, ev, re, r, rp)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard( function(c)
+            return c84130863.spFilter(c, e, SUMMON_TYPE_SPECIAL, tp, false, false, POS_FACEUP, tp)
+        end , tp, LOCATION_GRAVE, 0, 1, nil)
+    end
+end
+
+-- endregion
