707031e5be26c5713d2c5948e23c29e8068c5de5
SLAPaper <slapaper.pku@gmail.com>
2016-04-09 17:21:26 +0800

    fix (c84130862): make c84130862 work

--- MCGC/c84130862.lua
+++ MCGC/c84130862.lua
@@ -25,28 +25,32 @@ end
 
 function c84130862.activate(e, tp, eg, ep, ev, re, r, rp)
     local g = Duel.GetMatchingGroup( function(c)
-        return c:IsSetCard(0x2222) and c:IsImmuneToEffect(e)
+        return c:IsSetCard(0x2222) and not c:IsImmuneToEffect(e)
     end , tp, LOCATION_MZONE, 0, nil)
     local c = g:GetFirst()
     while c do
-        local e1 = Effect.CreateEffect(c)
+        local e1 = Effect.CreateEffect(e:GetHandler())
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_UPDATE_ATTACK)
         e1:SetValue(700)
         c:RegisterEffect(e1)
 
-        local e2 = Effect.CreateEffect(c)
+        local e2 = Effect.CreateEffect(e:GetHandler())
         e2:SetType(EFFECT_TYPE_SINGLE)
         e2:SetCode(EFFECT_DISABLE)
         e2:SetReset(RESET_EVENT + RESET_LEAVE + RESET_TODECK + RESET_TOGRAVE + RESET_TOHAND + RESET_TURN_SET + RESET_REMOVE)
         c:RegisterEffect(e2)
+
+        local e3 = Effect.CreateEffect(e:GetHandler())
+        e3:SetCode(EFFECT_DISABLE_EFFECT)
+        c:RegisterEffect(e3)
 
         c = g:GetNext()
     end
 end
 
 function c84130862.graveCondition(e, tp, eg, ep, ev, re, r, rp)
-    return Duel.GetTurnPlayer == tp and Card.GetTurnID() ~= Duel.GetTurnCount()
+    return Duel.GetTurnPlayer() == tp and e:GetHandler():GetTurnID() ~= Duel.GetTurnCount()
 end
 
 function c84130862.graveCost(e, tp, eg, ep, ev, re, r, rp, chk)
@@ -72,7 +76,7 @@ end
 function c84130862.graveOperation(e, tp, eg, ep, ev, re, r, rp)
     local g = Duel.GetChainInfo(0, CHAININFO_TARGET_CARDS)
     local c = g:GetFirst()
-    local e1 = Effect.CreateEffect(c)
+    local e1 = Effect.CreateEffect(e:GetHandler())
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetCode(EFFECT_UPDATE_ATTACK)
     e1:SetValue(500)
