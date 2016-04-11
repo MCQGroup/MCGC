02f2e63566bb46e199b88dc34330ed2c699d7a90
SLAPaper <slapaper.pku@gmail.com>
2016-04-11 23:03:01 +0800

    refactor (c84130863): fix wrong spsummon condition

--- MCGC/c84130863.lua
+++ MCGC/c84130863.lua
@@ -11,19 +11,23 @@ function c84130863.initial_effect(c)
     c:RegisterEffect(e1)
 end
 
+function c84130863.filter(c)
+    return c:IsSetCard(0x2222)
+end
+
 function c84130863.spFilter(c, e, sumtype, sumplayer, nocheck, nolimit, sumpos, target_player)
-    return c:IsSetCard(0x2222) and c:IsCanBeSpecialSummoned(e, sumtype, sumplayer, nocheck, nolimit, sumpos, target_player)
+    return c:IsCanBeSpecialSummoned(e, sumtype, sumplayer, nocheck, nolimit, sumpos, target_player)
 end
 
 function c84130863.target(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     if chk == 0 then
         return Duel.IsExistingMatchingCard( function(c)
-            return c:IsCanBeEffectTarget(e) and c84130863.spFilter(c, e, SUMMON_TYPE_SPECIAL, tp, false, false, POS_FACEUP, tp)
+            return c84130863.filter(c) and c:IsCanBeEffectTarget(e) and c84130863.spFilter(c, e, SUMMON_TYPE_SPECIAL, tp, false, false, POS_FACEUP, tp)
         end , tp, LOCATION_GRAVE, 0, 1, nil)
     end
     Duel.Hint(HINT_MESSAGE, tp, HINTMSG_SPSUMMON)
     local g = Duel.SelectMatchingCard( function(c)
-        return c:IsCanBeEffectTarget(e) and c84130863.spFilter(c, e, SUMMON_TYPE_SPECIAL, tp, false, false, POS_FACEUP, tp)
+        return c84130863.filter(c) and c:IsCanBeEffectTarget(e) and c84130863.spFilter(c, e, SUMMON_TYPE_SPECIAL, tp, false, false, POS_FACEUP, tp)
     end , tp, LOCATION_GRAVE, 0, 1, nil)
     Duel.SetTargetCard(g)
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, g, 1, nil, nil);
