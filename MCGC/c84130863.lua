d0442ba41a929255792334bc10f4438362c6ffe7
SLAPaper <slapaper.pku@gmail.com>
2016-04-13 23:16:57 +0800

    feat (c84130863): finish c84130863

--- MCGC/c84130863.lua
+++ MCGC/c84130863.lua
@@ -19,6 +19,17 @@ function c84130863.spFilter(c, e, sumtype, sumplayer, nocheck, nolimit, sumpos, 
     return c:IsCanBeSpecialSummoned(e, sumtype, sumplayer, nocheck, nolimit, sumpos, target_player)
 end
 
+function c84130863.xyzLimit(e, c)
+    if not c then
+        return false
+    end
+    return true
+end
+
+function c84130863.otherFilter(oc, c)
+    return(oc:GetLevel() == c:GetLevel() or oc:GetAttack() == c:GetAttack()) and not oc:IsCode(c:GetCode())
+end
+
 function c84130863.target(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     if chk == 0 then
         return Duel.IsExistingMatchingCard( function(c)
@@ -26,9 +37,9 @@ function c84130863.target(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
         end , tp, LOCATION_GRAVE, 0, 1, nil)
     end
     Duel.Hint(HINT_MESSAGE, tp, HINTMSG_SPSUMMON)
-    local g = Duel.SelectMatchingCard( function(c)
+    local g = Duel.SelectMatchingCard(tp, function(c)
         return c84130863.filter(c) and c:IsCanBeEffectTarget(e) and c84130863.spFilter(c, e, SUMMON_TYPE_SPECIAL, tp, false, false, POS_FACEUP, tp)
-    end , tp, LOCATION_GRAVE, 0, 1, nil)
+    end , tp, LOCATION_GRAVE, 0, 1, 1, nil)
     Duel.SetTargetCard(g)
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, g, 1, nil, nil);
 end
@@ -41,21 +52,29 @@ function c84130863.operation(e, tp, eg, ep, ev, re, r, rp)
         local e1 = Effect.CreateEffect(e:GetHandler())
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+        e1:SetValue(c84130863.xyzLimit)
         e1:SetReset(RESET_EVENT + RESET_LEAVE + RESET_TOGRAVE + RESET_TOHAND + RESET_REMOVE + RESET_TODECK)
         c:RegisterEffect(e1)
     end
-    Duel.Hint(HINT_MESSAGE, tp, HINTMSG_SPSUMMON)
-    local other_g = Duel.SelectMatchingCard(tp, function(oc)
-        return c84130863.spFilter(c, e, SUMMON_TYPE_SPECIAL, tp, false, false, POS_FACEUP, tp) and(oc:GetLevel() == c:GetLevel() or oc:GetAttack() == c:GetAttack()) and not oc:IsCode(c:GetCode())
-    end , tp, LOCATION_HAND + LOCATION_DECK, 0, 0, 1, nil)
-    local c2 = other_g:GetFirst()
-    local pos2 = Duel.SelectPosition(tp, c2, POS_FACEUP)
-    if Duel.SpecialSummon(c2, SUMMON_TYPE_SPECIAL, tp, tp, false, false, pos2) then
-        local e2 = Effect.CreateEffect(e:GetHandler())
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
-        e2:SetReset(RESET_EVENT + RESET_LEAVE + RESET_TOGRAVE + RESET_TOHAND + RESET_REMOVE + RESET_TODECK)
-        c2:RegisterEffect(e2)
+    if Duel.IsExistingMatchingCard( function(oc)
+            return c84130863.spFilter(oc, e, SUMMON_TYPE_SPECIAL, tp, false, false, POS_FACEUP, tp) and c84130863.otherFilter(oc, c)
+        end , tp, LOCATION_HAND + LOCATION_DECK, 0, 1, nil) then
+        Duel.Hint(HINT_MESSAGE, tp, HINTMSG_SPSUMMON)
+        local other_g = Duel.SelectMatchingCard(tp, function(oc)
+            return c84130863.spFilter(oc, e, SUMMON_TYPE_SPECIAL, tp, false, false, POS_FACEUP, tp) and c84130863.otherFilter(oc, c)
+        end , tp, LOCATION_HAND + LOCATION_DECK, 0, 0, 1, nil)
+        local c2 = other_g:GetFirst()
+        local pos2 = Duel.SelectPosition(tp, c2, POS_FACEUP)
+        if Duel.SpecialSummon(c2, SUMMON_TYPE_SPECIAL, tp, tp, false, false, pos2) then
+            local e2 = Effect.CreateEffect(e:GetHandler())
+            e2:SetType(EFFECT_TYPE_SINGLE)
+            e2:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
+            e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+            e2:SetValue(c84130863.xyzLimit)
+            e2:SetReset(RESET_EVENT + RESET_LEAVE + RESET_TOGRAVE + RESET_TOHAND + RESET_REMOVE + RESET_TODECK)
+            c2:RegisterEffect(e2)
+        end
     end
 end
 
